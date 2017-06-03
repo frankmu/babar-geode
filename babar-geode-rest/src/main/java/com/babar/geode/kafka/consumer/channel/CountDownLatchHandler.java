3f9dda7a605875a25f77153e8f841126fe702e71
frank <frankluck52@gmail.com>
2017-06-02 21:39:41 -0700

    remove unused countdownlatch

--- babar-geode-rest/src/main/java/com/babar/geode/kafka/consumer/channel/CountDownLatchHandler.java
+++ babar-geode-rest/src/main/java/com/babar/geode/kafka/consumer/channel/CountDownLatchHandler.java
@@ -1,22 +1,13 @@
 package com.babar.geode.kafka.consumer.channel;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.MessagingException;
 
 public class CountDownLatchHandler implements MessageHandler {
 
-	  private CountDownLatch latch = new CountDownLatch(10);
-
-	  public CountDownLatch getLatch() {
-	    return latch;
-	  }
-
-	  @Override
-	  public void handleMessage(Message<?> message) throws MessagingException {
-	    System.out.printf("received message='{%s}'\n", message);
-	    latch.countDown();
-	  }
+	@Override
+	public void handleMessage(Message<?> message) throws MessagingException {
+		System.out.printf("received message='{%s}'\n", message);
 	}
+}
