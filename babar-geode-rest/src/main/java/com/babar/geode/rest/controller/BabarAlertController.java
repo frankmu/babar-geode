58db0018e37f188493f9bd1a388092e0fae5273d
frank <frankluck52@gmail.com>
2017-07-29 14:51:59 -0700

    enable cross origin for controller

--- babar-geode-rest/src/main/java/com/babar/geode/rest/controller/BabarAlertController.java
+++ babar-geode-rest/src/main/java/com/babar/geode/rest/controller/BabarAlertController.java
@@ -1,6 +1,7 @@
 package com.babar.geode.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,11 +17,13 @@ public class BabarAlertController {
 	@Autowired
 	private AlertService alertService;
 
+	@CrossOrigin
 	@RequestMapping(value = "/alerts", method = RequestMethod.GET)
 	public Iterable<Alert> findAllOrderByTimestampLimit(@RequestParam(value = "limit", defaultValue = "50") String limit) {
 		return alertService.findAllOrderByAlertTimeDescLimit(Integer.parseInt(limit));
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/search/findByAlertTime", method = RequestMethod.GET)
 	public Iterable<Alert> findByAlertTimeGreaterThanAndAlertTimeLessThanOrderByAlertTimeDescLimit(
 			@RequestParam(value = "startTime", required = true) String startTime,
@@ -32,6 +35,7 @@ public class BabarAlertController {
 				Integer.parseInt(limit));
 	}
 
+	@CrossOrigin
 	@RequestMapping(value = "/alerts/{id}", method = RequestMethod.DELETE)
 	public Alert remove(@PathVariable("id") String id) {
 		return alertService.remove(id);
