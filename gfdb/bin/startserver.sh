b7b96379f0b597d32cf631ebe319582ac661765b
frank <frankluck52@gmail.com>
2017-07-31 21:32:22 -0700

    remove invalid gfdb

--- gfdb/bin/startserver.sh
+++
@@ -1,9 +0,0 @@
-export GFDB_HOME=/app/gfdb
-export GF_HOME=/app/gfdb
-export GF_JAVA=$JAVA_HOME/bin/java
-export GF_LIB=/app/gfdb/lib
-export CLASSPATH=$GF_HOME/lib/*:$GF_LIB/*
-export PATH=$GF_HOME/bin:$PATH
-export JAVA_ARGS="-Dgfsh.log-level=info -Dgfsh.log-dir=$GFDB_HOME/logs"
-
-gfsh start server --name=gfserver --properties-file=/app/gfdb/config/gemfire.properties --dir=/app/gfdb/gfserver --classpath=/app/gfdb/
--- gfdb/bin/stopserver.sh
+++
@@ -1,7 +0,0 @@
-export GF_HOME=/app/gfdb
-export GF_JAVA=$JAVA_HOME/bin/java
-export CLASSPATH=$GF_HOME/lib/*
-export PATH=$GF_HOME/bin:$PATH
-export JAVA_ARGS="-Dgfsh.log-level=info -Dgfsh.log-dir=$GF_HOME/logs"
-
-gfsh --e "connect --locator=localhost[30607]" --e "stop server --name=gfserver"
